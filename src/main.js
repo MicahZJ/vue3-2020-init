58e73b68fd230113fe8482235ed606113d124095
miko <miko117123@gmail.com>
2020-11-19 10:06:00 +0800

    vue3-init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 插件相关-element
+// element ui
+
+// 绑定axios到Vue原型链
+import HttpAxios from './utils/httpTool'
+import router from "./router";
+
+const app = createApp(App);
+
+app.config.globalProperties.$Http = HttpAxios;
+app.use(router);
+app.mount('#app');
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+
+// 构建我们的页面路由配置，可以看到，这里和原来的写法并无二致。
+const routes = [
+  {
+    path: '/',
+    component: HelloWorld
+  },
+  // {
+  //   path: '/about',
+  //   component: () => import('@/views/About.vue'),
+  // }
+];
+
+const router = createRouter({
+  // 使用 hash 模式构建路由（ url中带 # 号的那种)
+  // history: createWebHashHistory(),
+  // 使用 history 模式构建路由 （ url 中没有 # 号，但生产环境需要特殊配置）
+  history: createWebHistory(),
+  routes
+});
+export default router
