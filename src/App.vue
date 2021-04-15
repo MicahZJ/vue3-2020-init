bfb95ce4e45269b81d5a8e52076ee26e0489c071
miko <miko117123@gmail.com>
2021-04-15 13:54:20 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@ export default {
 .slide-fade-leave-active {
   transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
-.slide-fade-enter, .slide-fade-leave-to
+.slide-fade-enter-from, .slide-fade-leave-to
   /* .slide-fade-leave-active for below version 2.1.8 */ {
   transform: translateX(10px);
   opacity: 0;
