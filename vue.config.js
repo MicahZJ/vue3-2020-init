495f5c0d8ab145e8df4a47922b2af2261945d715
miko <miko117123@gmail.com>
2021-10-15 13:47:58 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   // 输出文件目录
@@ -22,6 +25,48 @@ module.exports = {
         esModule: false,
       })
       .end()
+  },
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production'){
+      // 定义压缩文件类型
+      const productionGzipExtensions = ['js', 'css', 'svg', 'ttf', 'otf']
+      config.plugins.push(
+        // 缓存dll,优化编译速度
+        new CompressionWebpackPlugin({
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          // test: /\.js$|\.html$|.\css/, //匹配文件名
+          threshold: 10240,//对超过10k的数据压缩
+          minRatio: 0.8,
+          deleteOriginalAssets: false //不删除源文件
+        })
+      )
+    }
+  
+    Object.assign(config, {
+      // 开发生产共同配置
+      externals: {
+        "BMap": "BMapGL",
+        'BMapLib':'BMapLib'
+      },
+      // externals: {
+      //   'vue': 'Vue',
+      //   'element-ui': 'ELEMENT',
+      //   'vue-router': 'VueRouter',
+      //   'vuex': 'Vuex'
+      // } // 防止将某些 import 的包(package)打包到 bundle 中，而是在运行时(runtime)再去从外部获取这些扩展依赖(用于csdn引入)
+      resolve: {
+        extensions: [".js", ".vue", ".json"], //文件优先解析后缀名顺序
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+          "@c": path.resolve(__dirname, "./src/components"),
+          "@v": path.resolve(__dirname, "./src/views"),
+          "@u": path.resolve(__dirname, "./src/utils"),
+          "@s": path.resolve(__dirname, "./src/service")
+        }, // 别名配置
+        plugins: [],
+      },
+    });
   },
   devServer: {
     disableHostCheck: true,
